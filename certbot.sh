a263241e12d0b9930d9c5bbe53acea241f085af3
deepzz0 <deepzz.qi@gmail.com>
2016-07-22 23:39:03 +0800

    update certbot

--- certbot.sh
+++ certbot.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+git clone https://github.com/letsencrypt/letsencrypt.git \
 cd letsencrypt \
 sudo ./certbot-auto certonly --standalone --email deepzz.qi@gmail.com \
   -d deepzz.com \
   -d www.deepzz.com \
   -d registry.deepzz.com 
+
+#./certbot-auto renew --standalone --pre-hook "service nginx stop" --post-hook "service nginx start"
